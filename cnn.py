910d947ae3be3ced9a5eefc2afaab7413b490711
Rui Lin <linrui@qingtingfm.com>
2017-12-10 18:01:21 -0500

    first commit

---
+++ cnn.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+################ initialization ###############
+import os
+
+# -- torch
+import torch
+from torch import autograd
+from torch import nn
+from torch.nn import functional as F
+from torch import optim
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, PackedSequence
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+torch.manual_seed(42)
+Var = autograd.Variable
+T = torch.Tensor
+
+# -- PIL
+from PIL import Image
+
+# -- logging
+import logging
+import logging.handlers
+LOG_LEVEL = logging.DEBUG
+
+def logging_config():
+    root_logger = logging.getLogger()
+    root_logger.setLevel(LOG_LEVEL)
+
+    f = logging.Formatter("[%(levelname)s]%(module)s->%(funcName)s: \t %(message)s \t --- %(asctime)s")
+
+    h = logging.StreamHandler()
+    h.setFormatter(f)
+    h.setLevel(LOG_LEVEL)
+
+    root_logger.addHandler(h)
+
+
+# -- argparse
+import argparse
+
+def parse_cmd():
+    parser = argparse.ArgumentParser(description='CNN for EMNIST')
+    parser.add_argument('--data_dir', type=str, default='EMNIST20')
+    parser.add_argument('--num_per_class_training', type=int, default=None)
+    parser.add_argument('--num_per_class_testing', type=int, default=None)
+    parser.add_argument('--batch_size', type=int, default=40)
+    parser.add_argument('--epochs', type=int, default=10)
+    args = parser.parse_args()
+    return args
+
+
+# -- dataset
+class EMNIST(Dataset):
+    def __init__(self, root_dir, num_per_class=None, transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])):
+        self.root_dir = root_dir
+        self.num_per_class = num_per_class
+        self.transform = transform
+        self.paths = []
+        self.cls_list = sorted(os.listdir(root_dir))
+        self.num_cls = len(self.cls_list)
+        for cls in self.cls_list:
+            for img in sorted(os.listdir(os.path.join(self.root_dir, cls)))[:self.num_per_class]:
+                self.paths.append((cls, img))
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir, *self.paths[idx])
+        image = Image.open(img_name)
+        label = self.cls_list.index(self.paths[idx][0])
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+
+# -- convnet, use the official pytorch example as reference
+# (https://github.com/pytorch/examples/blob/master/mnist/main.py)
+class Net(nn.Module):
+    def __init__(self, num_cls):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d() # avoid overfitting
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, num_cls)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+
+# -- training
+def train1epoch(model, loader):
+    model.train()
+    total_loss = 0
+    batch_cnt = 0
+    for batch_idx, (data, target) in enumerate(loader):
+        if torch.cuda.is_available():
+            data, target = data.cuda(), target.cuda()
+        data, target = Var(data), Var(target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.data[0]
+        batch_cnt += 1
+
+        # release GPU memory
+        del loss
+        del output
+        del data
+        del target
+
+    return total_loss / batch_cnt
+
+
+# -- testing
+def test(model, loader):
+    model.eval() ## set Dropout / BatchNorm in evaluation mode
+    test_loss = 0
+    batch_cnt = 0
+    correct = 0
+    for data, target in loader:
+        if torch.cuda.is_available():
+            data, target = data.cuda(), target.cuda()
+        data, target = Var(data, volatile=True), Var(target)
+        output = model(data)
+
+        test_loss += F.nll_loss(output, target).cpu().data[0] # sum up average batch loss
+        batch_cnt += 1
+
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+        # release GPU memory
+        del pred
+        del output
+        del data
+        del target
+
+    return test_loss / batch_cnt, correct / len(loader.dataset)
+
+
+if __name__ == "__main__":
+    logging_config()
+    logger = logging.getLogger()
+
+    args = parse_cmd()
+
+    # data set
+    dir_training = os.path.join(args.data_dir, 'training')
+    dir_testing = os.path.join(args.data_dir, 'testing')
+    train_loader = DataLoader(EMNIST(dir_training, args.num_per_class_training), batch_size=args.batch_size, shuffle=True, num_workers=4)
+    test_loader = DataLoader(EMNIST(dir_testing, args.num_per_class_training), batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+    # model
+    model = Net(train_loader.dataset.num_cls)
+    if torch.cuda.is_available():
+        model.cuda()
+
+    # training
+    optimizer = optim.Adam(model.parameters()) # using default lr=0.001, betas=(0.9, 0.999)
+    for e in range(args.epochs):
+        loss = train1epoch(model, train_loader)
+        logger.info("Epoch %s: %s", e, loss)
+        if e % 5 == 0:
+            loss, acc = test(model, test_loader)
+            logger.info("test loss: %s, test accuracy: %s", loss, acc)
+
---
+++ rcn.py
@@ -0,0 +1,124 @@
+#! /usr/bin/env python2.7
+import argparse
+import numpy as np
+import os
+from multiprocessing import Pool
+from functools import partial
+from scipy.misc import imresize
+from scipy.ndimage import imread
+
+from science_rcn.inference import test_image
+from science_rcn.learning import train_image
+
+np.random.seed(42)
+
+# -- logging
+import logging
+import logging.handlers
+LOG_LEVEL = logging.DEBUG
+
+def logging_config():
+    root_logger = logging.getLogger()
+    root_logger.setLevel(LOG_LEVEL)
+
+    f = logging.Formatter("[%(levelname)s]%(module)s->%(funcName)s: \t %(message)s \t --- %(asctime)s")
+
+    h = logging.StreamHandler()
+    h.setFormatter(f)
+    h.setLevel(LOG_LEVEL)
+
+    root_logger.addHandler(h)
+
+# -- argparse
+def parse_cmd():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='EMNIST20')
+    parser.add_argument(
+        '--num_per_class_training',
+        type=int,
+        default=None,
+        help="Number of training examples.",
+    )
+    parser.add_argument(
+        '--num_per_class_testing',
+        type=int,
+        default=None,
+        help="Number of testing examples.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+# -- rcn, use the rcn MNIST example code as reference
+def run(data_dir,
+        num_per_class_training=None,
+        num_per_class_testing=None,
+        pool_shape=(25, 25), # use default
+        perturb_factor=2., # use default
+        ):
+    LOG = logging.getLogger()
+
+    # Multiprocessing set up
+    pool = Pool(4)
+
+    train_data, test_data = get_mnist_data_iters(
+        data_dir, num_per_class_training, num_per_class_testing)
+
+    LOG.info("Training on {} images...".format(len(train_data)))
+    train_partial = partial(train_image,
+                            perturb_factor=perturb_factor)
+    train_results = pool.map_async(train_partial, [d[0] for d in train_data]).get(9999999)
+    all_model_factors = zip(*train_results)
+
+    LOG.info("Testing on {} images...".format(len(test_data)))
+    test_partial = partial(test_image, num_candidates=5, model_factors=all_model_factors,
+                           pool_shape=pool_shape) # less number of candidates
+    test_results = pool.map_async(test_partial, [d[0] for d in test_data]).get(9999999)
+
+    # Evaluate result
+    correct = 0
+    for test_idx, (winner_idx, _) in enumerate(test_results):
+        correct += test_data[test_idx][1] == train_data[winner_idx][1]
+    print "Total test accuracy = {}".format(float(correct) / len(test_results))
+
+    return all_model_factors, test_results
+
+
+def get_mnist_data_iters(data_dir, num_per_class_training, num_per_class_testing):
+    if not os.path.isdir(data_dir):
+        raise IOError("Can't find your data dir '{}'".format(data_dir))
+
+    def _load_data(image_dir, num_per_class, get_filenames=False):
+        loaded_data = []
+        for category in sorted(os.listdir(image_dir)):
+            cat_path = os.path.join(image_dir, category)
+            if not os.path.isdir(cat_path) or category.startswith('.'):
+                continue
+            samples = sorted(os.listdir(cat_path))[:num_per_class]
+
+            for fname in samples:
+                filepath = os.path.join(cat_path, fname)
+                # Resize and pad the images to (200, 200)
+                image_arr = imresize(imread(filepath), (112, 112))
+                img = np.pad(image_arr,
+                             pad_width=tuple([(p, p) for p in (44, 44)]),
+                             mode='constant', constant_values=0)
+                loaded_data.append((img, category))
+        return loaded_data
+
+    train_set = _load_data(os.path.join(data_dir, 'training'),
+                           num_per_class=num_per_class_training)
+    test_set = _load_data(os.path.join(data_dir, 'testing'),
+                          num_per_class=num_per_class_testing)
+    return train_set, test_set
+
+
+if __name__ == '__main__':
+    logging_config()
+    logger = logging.getLogger()
+
+    args = parse_cmd()
+
+    run(args.data_dir,
+            num_per_class_training=args.num_per_class_training,
+            num_per_class_testing=args.num_per_class_testing)
